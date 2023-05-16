84d3eb52c46742ecc4af2083cf57090e251042ac
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-05-16 12:20:16 -0500

    portafolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,60 +1,69 @@
-import React from 'react'
-import IMG1 from '../../assets/porta1.PNG'
-import IMG2 from '../../assets/porta2.PNG'
-import IMG3 from '../../assets/porta3.PNG'
-import './portfolio.css'
+import React from "react";
+import IMG1 from "../../assets/porta1.PNG";
+import IMG2 from "../../assets/porta2.PNG";
+import IMG3 from "../../assets/porta3.PNG";
+import IMG4 from "../../assets/porta4.PNG";
+import "./portfolio.css";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Dogs App',
-    github: 'https://github.com/EmmanuelML78/PI-Dogs',
-    demo: 'https://pi-dogs-lilac.vercel.app/'
+    title: "Dogs App",
+    github: "https://github.com/EmmanuelML78/PI-Dogs",
+    demo: "https://pi-dogs-lilac.vercel.app/",
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Rick and Morty',
-    github: 'https://github.com/EmmanuelML78/rick_and_morty',
-    demo: 'https://rick-and-morty-delta-seven.vercel.app'
+    title: "Rick and Morty",
+    github: "https://github.com/EmmanuelML78/rick_and_morty",
+    demo: "https://rick-and-morty-delta-seven.vercel.app",
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Snake',
-    github: 'https://github.com/EmmanuelML78/game-2',
-    demo: 'https://game-seven-eta.vercel.app/'
+    title: "Snake",
+    github: "https://github.com/EmmanuelML78/game-2",
+    demo: "https://game-seven-eta.vercel.app/",
   },
- 
-]
+  {
+    id: 4,
+    image: IMG4,
+    title: "El Predio",
+    github: "https://github.com/EmmanuelML78/EL-PREDIO",
+    demo: "https://el-predio.vercel.app/",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          data.map(({id, image, title, github, demo}) => {
-            return (
-              <article key={id} className='portfolio__item'>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-              <a href={github} className='btn' target='_blank'>Github</a>
-              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
               </div>
-              </article>
-            )
-          })
-        }
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
