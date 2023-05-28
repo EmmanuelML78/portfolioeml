82a4471624849dd7a3901d74ac155bb6151ab17b
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-05-27 20:37:26 -0500

    actualizo

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -31,9 +31,9 @@ const AnimatedText = ({ className = "", text }) => {
   return (
     <div
       className="w-full mx-auto py-2 items-center justify-center text-center overflow-hidden
-      sm:py-0 ">
+      sm:py-0  ">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-7xl dark:text-light  ${className}`}
+        className={`inline-block w-full text-dark font-bold capitalize xs:text-4xl text-7xl dark:text-light  ${className}`}
         variants={quote}
         initial="initial"
         animate="animate">
